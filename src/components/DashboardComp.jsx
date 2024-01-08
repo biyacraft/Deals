5a0c7d9655d022a0f67db4dbc7c208c4ae26fce9
abbyyt <abitesfahunn@gmail.com>
2024-01-08 23:20:53 +0300

    update

--- src/components/DashboardComp.jsx
+++ src/components/DashboardComp.jsx
@@ -12,30 +12,28 @@ const DashboardComp = () => {
           </div>
        </div>
       </div>
-    <div className="flex">
-      <div className="flex-col">
-      <div className="flex w-full">
-        <div className="w-1/3 flex justify-between items-center ">
+    <div className="flex justify-between">
+      <div className="flex-col w-1/2">
+      
+        <div className="flex justify-between items-center pr-8">
           <div className="text-gray-600 text-bold">
             <p className="p-1 my-3">Customers</p>
             <h1 className="p-1 my-3 text-6xl font-bold text-secondary">78</h1>
           </div>
           <div className="p-1 pt-3 items-center">
-            <img className="w-20 h-20 pt-3 mt-3" src="customers.svg" alt="" />
+            <img className="w-40 h-40 pt-3 mt-3" src="customers.svg" alt="" />
           </div>
-        </div>
-        
       </div>
-      <div className="flex w-full">
-        <div className="w-1/3 flex justify-between items-center ">
+
+      < div className="flex justify-between items-center pr-8">
+        
           <div className="text-gray-600 text-bold">
             <p className="p-1 my-3">Deals</p>
             <h1 className="p-1 my-3 text-6xl font-bold text-secondary">136</h1>
           </div>
           <div className="p-1 pt-3 items-center">
-            <img className="w-20 h-20 pt-3 mt-3" src="customers-shop.svg" alt="" />
+            <img className="w-40 h-40 pt-3 mt-3" src="customers-shop.svg" alt="" />
           </div>
-        </div>
       </div>
       </div>
       <div className="w-2/3">
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -7,8 +7,64 @@ const SideBar = () => {
     return (
     <div className="flex">
         <SideBarr />
-            <DashboardComp />
+        <div className="w-2/3">
+        <DashboardComp />
         </div>
+        <div className="w-1/3 p-4">
+        <div className="flex-col">
+            <div className="flex justify-between">
+                <h1 className="text-secondary font-bold">Customers</h1>
+                <button className="text-gray-600">View All</button>
+            </div>
+        <div className="flex-col">
+            <div className="flex items-center gap-4 my-4">
+        <div className="">
+            <img className="rounded-full w-12 h-12" src="user.png" alt="" />
+        </div>
+        <div className="w-full">
+            <h2 className="text-secondary font-bold">Eyob Abate</h2>
+            <div className="flex text-gray-600 items-start justify-between">
+            <p>eyobabate@gmail.com</p>
+            <button>
+                <img src="edit-button.svg" alt="" />
+            </button>
+            </div>
+        </div>
+        </div>
+        <div className="flex items-center gap-4 my-4">
+        <div className="">
+            <img className="rounded-full w-12 h-12" src="user.png" alt="" />
+        </div>
+        <div className="w-full">
+            <h2 className="text-secondary font-bold">Gashewu Meketa</h2>
+            <div className="flex text-gray-600 items-start justify-between">
+            <p>gashawmeketa@gmail.com</p>
+            <button>
+                <img src="edit-button.svg" alt="" />
+            </button>
+            </div>
+        </div>
+        </div>
+        <div className="flex items-center gap-4 my-4">
+        <div className="">
+            <img className="rounded-full w-12 h-12" src="user.png" alt="" />
+        </div>
+        <div className="w-full">
+            <h2 className="text-secondary font-bold">Brihane Hailay</h2>
+            <div className="flex text-gray-600 items-start justify-between">
+            <p>BrihaneHailay@gmail.com</p>
+            <button>
+                <img src="edit-button.svg" alt="" />
+            </button>
+            </div>
+        </div>
+        </div>
+            </div>
+        </div>
+        <div className="tasksTODo">
+        </div>
+        </div>
+    </div>
   )
 }
 
