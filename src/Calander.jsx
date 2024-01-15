01bbe5621809329337502cff8b1b78eb63b83178
abbyyt <abitesfahunn@gmail.com>
2024-01-16 00:52:19 +0300

    Update

--- src/Calander.jsx
+++ src/Calander.jsx
@@ -29,18 +29,19 @@ const Calander = () => {
             <SideBar />
             <div className="flex-col">
                 <h2 className="pl-6 text-gray-600">Here are the list of the projects you have been working on</h2>
-                
-                    <div className="flex-col p-4">
-                        <div className="flex justify-between">
-                            <div className="flex w-2/3 items-center gap-3">
+                <div className="flex-col p-4">
+                    <div className="flex justify-between">
+                        <div className="flex w-2/3 items-center gap-3">
                             <h1 className="text-secondary text-2xl font-bold">February 2021</h1>
+                            <select name="" id="">
                             <img src="scroll.svg" alt="" />
-                            </div>
-                            <div className="flex gap-2 text-white bg-primary rounded-xl justify-center items-center p-3">
-                                <h2>Create</h2>
-                                <img src="add.svg" alt="" />
-                            </div>
+                            </select>
                         </div>
+                        <div className="flex gap-2 text-white bg-primary rounded-xl justify-center items-center p-3">
+                            <h2>Create</h2>
+                            <img src="add.svg" alt="" />
+                        </div>
+                    </div>
                         <div className=" text-secondary pt-6">
                             <table className="w-full">
                                 <tr className="text-secondary font-bold flex justify-between p-3 items-start">
@@ -53,13 +54,13 @@ const Calander = () => {
                                     <td>Sun</td>
                                 </tr>
                                 <tr className="text-secondary font-semibold flex justify-between p-3 items-start">
-                                    <td>1</td>
-                                    <td>2</td>
-                                    <td>3</td>
-                                    <td>4</td>
-                                    <td>5</td>
-                                    <td>6</td>
-                                    <td>7</td>
+                                    <div className=""><td>1</td></div>
+                                    <div><td>2</td></div>
+                                    <div><td>3</td></div>
+                                    <div><td>1</td></div>
+                                    <div><td>12</td></div>
+                                    <div><td>1</td></div>
+                                    <div><td>1</td></div>
                                 </tr>
                                 <tr className="text-secondary font-semibold flex justify-between p-3 items-start">
                                     <td>8</td>
@@ -73,7 +74,7 @@ const Calander = () => {
                                 <tr className="text-secondary font-semibold flex justify-between p-3 items-start">
                                     <td>15</td>
                                     <td>16</td>
-                                    <td>17</td>
+                                    <td className="">17</td>
                                     <td>18</td>
                                     <td>19</td>
                                     <td>20</td>
@@ -99,9 +100,19 @@ const Calander = () => {
                                 </tr>
                             </table>
                         </div>
+                        <div className="flex gap-6">
+                            <div className="flex gap-3">
+                                <div className="w-6 h-6 flex bg-green-600 rounded-full p-2"></div>
+                                <div>Event Dates</div>
+                            </div>
+                            <div className="flex gap-3">
+                                <div className="w-6 h-6 flex bg-blue-600 rounded-full p-2"></div>
+                                <div>Selected Date</div>
+                            </div>
+                        </div>
                     </div>
-                    <div></div>
-                    <div></div>
+                <div></div>
+                <div></div>
             </div>
         </div>
     </div>
