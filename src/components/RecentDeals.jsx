a396da546d6c5501421cdafc40bb82d88d5b9b67
Abi tesfa <abitesfahunn@gmail.com>
2024-02-28 17:09:42 +0300

    update

--- src/components/RecentDeals.jsx
+++ src/components/RecentDeals.jsx
@@ -1,28 +1,6 @@
 
 const RecentDeals = () => {
-<<<<<<< HEAD
     return (
-        <div className="p-4 bg-gray-100 rounded-2xl">
-            <div className="flex items-start justify-between p-1">
-                <h1 className="text-secondary font-bold">Recent Deals</h1>
-                <button className="text-gray-600">Veiw All</button>
-            </div>
-            <div>
-                <div className="flex items-center gap-4 my-4">
-                    <div className="">
-                        <img className="rounded-full w-12 h-12" src="user.png" alt="" />
-                    </div>
-                    <div className="w-full">
-                        <h2 className="text-secondary font-bold">Lorem, ipsum.</h2>
-                        <div className="flex text-gray-600 items-start justify-between">
-                            <p>Lorem, ipsum.</p>
-                            <p>Nov 14, 07:00AM</p>
-                        </div>
-                    </div>
-                </div>
-                <div className="flex items-center gap-4 my-4">
-=======
-  return (
     <div className="p-4 bg-gray-100 rounded-2xl">
         <div className="flex items-start justify-between p-1">
             <h1 className="text-secondary font-bold">Recent Deals</h1>
@@ -31,7 +9,6 @@ const RecentDeals = () => {
         <div>
             <div className="flex items-center gap-4 my-4">
                 <div className="">
->>>>>>> 97dd3269b0308461fdc550e3c1feb9a42143a451
                     <img className="rounded-full w-12 h-12" src="user.png" alt="" />
                     <div className="w-full">
                         <h2 className="text-secondary font-bold">Lorem, ipsum.</h2>
@@ -62,7 +39,7 @@ const RecentDeals = () => {
                     </div>
                 </div>
             </div>
-        </div>
+            </div></div>
     )
 }
 
